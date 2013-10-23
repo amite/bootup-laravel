b760da2ac45d9b7e9e4cdb6f898e5a7fe43ac28c
amite <amit.erandole@gmail.com>
2013-10-23 10:10:36 +0530

    updated base layout

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,6 +1,11 @@
-<div class="container" style="margin-top:40px">
-	<div class="jumbotron">
-		<h1>Hello !</h1>
-		<p id="home-content" style="display:none;">This content is load via ajax request</p>
-	</div>
+@extends('layouts.default')
+
+@section('content')
+<div class="container">
+  <div class="jumbotron">
+    <h1>Hello !</h1>
+    <p id="home-content" style="display:none;">This content is load via ajax request</p>
+  </div>
 </div>
+
+@stop
